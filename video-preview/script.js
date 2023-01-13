5666f35723f9354f1d6a879d4121273e83c42820
nyezza <yezza.nadhir@gmail.com>
2023-01-13 15:28:45 +0100

    try1

--- video-preview/script.js
+++ video-preview/script.js
@@ -1,2 +1,5 @@
 console.log("page loaded...");
 //xmlns="http://www.w3.org/2000/svg"
+function play(x)
+    var x = document.getElementById(id);
+    document.getElementById(id).innerText= "start:"+ x.played.start(0) + "end:" + x.played.end(0);
