38215171393b96416a29b115a86f5192663f5aa0
nyezza <yezza.nadhir@gmail.com>
2023-01-18 17:31:52 +0100

    querry

--- DojoWeather/script.js
+++ DojoWeather/script.js
@@ -6,12 +6,19 @@ function RmPopup(){
     document.querySelector(".footer").remove();
 }
 
-function ConvertTmp(id){
-    var x= document.getElementById(id).innerText;
+function ConvertTmp(){
+    var x= document.querySelector("#tday").innerText;
     console.log(x);
+    var yy= document.querySelector("#night").innerText;
+    console.log(x);
+/*
+    var y = document.getElementById("temp-select").innerText;
+    console.log(y);
     if ((document.getElementById("temp-select")) == '°F'){
-        var y= x + 32;
-        console.log(y);
+        /*var f=32;
+        var z =sum(x,f);
+        console.log(z);
+        x++;
     }
-    document.getElementById(id).innerText=y;
+    document.getElementById(id).innerText=x;*/
 }
