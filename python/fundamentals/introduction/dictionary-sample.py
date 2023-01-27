2288eda50bcda46e16c2544143f3bca29e8dbd65
nyezza <yezza.nadhir@gmail.com>
2023-01-27 06:55:53 +0100

    dictinary

---
+++ python/fundamentals/introduction/dictionary-sample.py
@@ -0,0 +1,15 @@
+# weekend = {"Sun": "Sunday", "Sat": "Saturday"} #literal notation
+# capitals = {} #create an empty dictionary then add values
+# capitals["svk"] = "Bratislava"
+# capitals["deu"] = "Berlin"
+# capitals["dnk"] = "Copenhagen"
+
+# print(capitals)
+# print(weekend["Sun"])
+# print(capitals["svk"])
+# value_removed = capitals.pop('svk') # will remove the key 'svk' and return it's value
+# del capitals['dnk'] # will delete the key, and not return anything
+# print(capitals)
+for count in range(0,5):
+    print("looping =", count)
+
