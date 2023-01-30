029eb7db464c7d22bdfc1d9406532d01e10d244a
nyezza <yezza.nadhir@gmail.com>
2023-01-30 07:02:43 +0100

    loop

---
+++ python/fundamentals/fundamentals/for_loop_basic1.py
@@ -0,0 +1,13 @@
+# for x in range (0,151,1):
+#     if x%10==0:
+#         print("Coding Dojo")
+#         continue
+#     elif x%5==0:
+#         print("Coding")
+#         continue
+#     else :
+#         print(x)
+    
+# for y in range (0,5000)
+    
+print (type(55))
