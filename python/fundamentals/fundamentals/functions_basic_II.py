abc50f34f9949f879ef953abbc60ab73f9d5afb9
nyezza <yezza.nadhir@gmail.com>
2023-01-31 17:46:36 +0100

    vf

---
+++ python/fundamentals/fundamentals/functions_basic_II.py
@@ -0,0 +1,43 @@
+# 1 Countdown
+
+# def CountDown(m):
+#     coun=[]
+#     for x in range(m,-1,-1):
+#         coun.append(x)
+#     print (coun)
+# CountDown(5)
+
+# 2 Print and Return 
+# def print_and_return(a,b):
+#     print(a)
+#     return b
+# print_and_return(1,2)
+
+# 3 First Plus Length
+# def first_plus_length(m):
+#     b=len(m)
+#     a=m[0]
+#     print(a+b) 
+# first_plus_length([100,20,5,4,3,10])
+
+# 4 Values Greater than Second
+# def values_greater_than_second(m):
+#     g= m[1]
+#     new_list=[]
+#     for x in m:
+#         if x > g:
+#             new_list.append(x)
+#     if len(new_list) < 2:
+#         return False
+#     else:
+#         print(len(new_list))
+#         return new_list
+# values_greater_than_second([5,2,3,2,1,4])
+
+# This Length, That Value
+# def length_and_value(a,b):
+#     nlist=[]
+#     for x in range (0,a):
+#         nlist.append(b)
+#     print(nlist)
+# length_and_value(4,7)
