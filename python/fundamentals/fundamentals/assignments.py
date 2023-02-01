9438af61cd2aa1a51c4e93dcf9a7ee2c9dc9423b
nyezza <yezza.nadhir@gmail.com>
2023-02-01 07:02:57 +0100

    1

---
+++ python/fundamentals/fundamentals/assignments.py
@@ -0,0 +1,18 @@
+# 1- Update Values in Dictionaries and Lists
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0]=15
+print(x[1][0])
+students[0]['last_name']='Bryant'
+print(students[0]['last_name'])
+z[0]['y']=30
+print(z)
