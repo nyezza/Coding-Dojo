30943796be9e76c94dbf03bca2136ed8085b3ce5
nyezza <yezza.nadhir@gmail.com>
2023-02-02 14:18:54 +0100

    vf

--- python/fundamentals/oop/user2.py
+++ python/fundamentals/oop/user2.py
@@ -3,13 +3,18 @@ class User:
         self.name= name
         self.email= email 
         self.account_balance=0
-        
     def make_deposit(self,amount):
-        self.account_balance += amount
-        
+        if amount<0:
+            print("there a prolem, you can't deposit a negativ amount")
+        else:    
+            self.account_balance += amount
+        return self
     def make_withdrawal(self, amount):
-        self.account_balance -= amount
-        
+        if amount<0:
+            print("there a prolem, you can't withdrawal a negativ amount")
+        else:
+            self.account_balance -= amount
+        return self
     def display_user_balance(self):
         print (f"User: {self.name}, Balance: ${self.account_balance}")
         
@@ -32,21 +37,11 @@ nadhir.make_withdrawal(1000)
 print(f"the new balance after the withdrawal {nadhir.account_balance}")
 nadhir.display_user_balance()
 
-nadhir.make_deposit(500)
-nadhir.make_deposit(500)
-nadhir.make_deposit(500)
-nadhir.make_withdrawal(1000)
-nadhir.display_user_balance()
 
-aymen.make_deposit(500)
-aymen.make_deposit(500)
-aymen.make_withdrawal(1000)
-aymen.display_user_balance()
+nadhir.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawal(200).display_user_balance()
 
-nouha.make_deposit(500)
-nouha.make_withdrawal(1000)
-nouha.make_withdrawal(1000)
-nouha.make_withdrawal(1000)
-nouha.display_user_balance()
+aymen.make_deposit(100).make_deposit(100).make_withdrawal(200).display_user_balance()
+
+nouha.make_deposit(100).make_withdrawal(-100).make_withdrawal(-200).display_user_balance()
 
 nadhir.transfer_money(nouha,100)
