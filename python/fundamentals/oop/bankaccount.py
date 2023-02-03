4c93b0efd14c95cc22248b9d749d6a29102a0198
nyezza <yezza.nadhir@gmail.com>
2023-02-03 08:33:10 +0100

    vf

--- python/fundamentals/oop/bankaccount.py
+++ python/fundamentals/oop/bankaccount.py
@@ -1,7 +1,9 @@
 class BankAccount:
+    costumers_account=[]
     def __init__(self, int_rate, balance): 
         self.int_rate=0.01
         self.balance=0
+        BankAccount.costumers_account.append(self)
     def deposit(self, amount):
         self.balance += amount
         return self
@@ -14,13 +16,20 @@ class BankAccount:
         return self
     def display_account_info(self):
         print(f"The Balance: {self.balance}")
+        return self
     def yield_interest(self):
         if self.balance>0:
             self.balance += (self.balance * self.int_rate)
         return self
+    @classmethod
+    def Account_inventory(cls):
+        for ac in cls.costumers_account:
+            ac.display_account_info()
 
 nadhir_account= BankAccount(0.01,0)
 nouha_account=BankAccount(0.01,0)
 
 nadhir_account.deposit(100).deposit(100).deposit(1000).withdraw(200).yield_interest().display_account_info()
 nouha_account.deposit(100).deposit(100).withdraw(50).withdraw(50).withdraw(100).withdraw(100).yield_interest().display_account_info()
+
+BankAccount.Account_inventory()
