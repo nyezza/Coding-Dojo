a1753ba18d91d6e735c364299b05acc025cdb22c
nyezza <yezza.nadhir@gmail.com>
2023-02-03 06:56:59 +0100

    \bonus

--- python/fundamentals/oop/bankaccount.py
+++ python/fundamentals/oop/bankaccount.py
@@ -1,4 +1,26 @@
 class BankAccount:
-    def __init__(self,balance):
+    def __init__(self, int_rate, balance): 
+        self.int_rate=0.01
         self.balance=0
-        
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    def withdraw(self, amount):
+        if self.balance < amount:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+        else:
+            self.balance -= amount
+        return self
+    def display_account_info(self):
+        print(f"The Balance: {self.balance}")
+    def yield_interest(self):
+        if self.balance>0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+
+nadhir_account= BankAccount(0.01,0)
+nouha_account=BankAccount(0.01,0)
+
+nadhir_account.deposit(100).deposit(100).deposit(1000).withdraw(200).yield_interest().display_account_info()
+nouha_account.deposit(100).deposit(100).withdraw(50).withdraw(50).withdraw(100).withdraw(100).yield_interest().display_account_info()
