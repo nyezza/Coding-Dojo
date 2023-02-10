15a59d95bed119a4446b6d5cc70fb5b5371009f7
nyezza <yezza.nadhir@gmail.com>
2023-02-10 15:58:08 +0100

    vf

--- python/fundamentals/oop/BankAccount/bank_account.py
+++ python/fundamentals/oop/BankAccount/bank_account.py
@@ -1,11 +1,32 @@
+
 class BankAccount:
     Bank_name='BIAT'
     accounts=[]
     account_default_id=0
-    def __init__(self,balance,int_rate):
+    def __init__(self,balance=0,int_rate=0.02):
         BankAccount.account_default_id += 1
         self.id= BankAccount.account_default_id
-        self.balance=0
-        self.int_rate=0.02
+        self.balance=balance
+        self.int_rate=int_rate
         BankAccount.accounts.append(self)
-        
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    def withdraw(self, amount):
+        if self.balance < amount:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+        else:
+            self.balance -= amount
+        return self
+    def display_account_info(self):
+        print(f"The Balance: {self.balance}")
+        return self
+    def yield_interest(self):
+        if self.balance>0:
+            self.balance += (self.balance * self.int_rate)
+        return self
+    @classmethod
+    def change_bank(cls, new_bank):
+        cls.Bank_name=new_bank
+    
--- python/fundamentals/oop/BankAccount/user.py
+++ python/fundamentals/oop/BankAccount/user.py
@@ -1,6 +1,12 @@
+from bank_account import BankAccount
 class User:
     def __init__(self,name,email,age):
         self.name=name
         self.email=email
         self.age=age
-    
+        self.account=[BankAccount.accounts()]
+
+n_account=BankAccount(1000)
+nadhir=User('Nadhir','yezza.nadhir@gmail.com',36)
+print(n_account.Bank_name,n_account.balance)
+print(nadhir.account)
