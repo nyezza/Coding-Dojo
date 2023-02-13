5da72dc84ba4b3e09ed3bf6b39c2fd60ae5c3a57
nyezza <yezza.nadhir@gmail.com>
2023-02-13 17:25:59 +0100

    vf

--- python/fundamentals/oop/ninjas_vs_pirates/classes/pirate.py
+++ python/fundamentals/oop/ninjas_vs_pirates/classes/pirate.py
@@ -6,8 +6,8 @@ class Pirate:
         self.speed = 3
         self.health = 100
 
-    def show_stats( self ):
-        print(f"Name: {self.name}\nStrength: {self.strength}\nSpeed: {self.speed}\nHealth: {self.health}\n")
+    # def show_stats( self ):
+    #     print(f"Name: {self.name}\nStrength: {self.strength}\nSpeed: {self.speed}\nHealth: {self.health}\n")
 
     def attack ( self , ninja ):
         ninja.health -= self.strength
--- python/fundamentals/oop/ninjas_vs_pirates/game.py
+++ python/fundamentals/oop/ninjas_vs_pirates/game.py
@@ -1,9 +1,32 @@
 from classes.ninja import Ninja
 from classes.pirate import Pirate
+class Game:
+    def __init__(self):
+        pass
+    def show_stats( self ):
+        print(f"Name: {self.name}\nStrength: {self.strength}\nSpeed: {self.speed}\nHealth: {self.health}\n")    
 
 michelangelo = Ninja("Michelanglo")
 
 jack_sparrow = Pirate("Jack Sparrow")
 
-michelangelo.attack(jack_sparrow)
-jack_sparrow.show_stats()
+# michelangelo.attack(jack_sparrow).show_stats()
+# jack_sparrow.attack(michelangelo).show_stats()
+
+# michelangelo.attack(jack_sparrow)
+# Game.show_stats(jack_sparrow)
+
+raff=Ninja("Raff")
+gibs=Pirate("Gibs")
+
+while raff.health >0 or gibs.health>0:
+    raff.attack(gibs)
+    Game.show_stats(raff)
+    Game.show_stats(gibs)
+    gibs.attack(raff)
+    Game.show_stats(raff)
+    Game.show_stats(gibs)
+if gibs.health> raff.health:
+    print(f"{gibs.name} is the winner")
+else:
+    print(f"{raff.name} is the winner")
