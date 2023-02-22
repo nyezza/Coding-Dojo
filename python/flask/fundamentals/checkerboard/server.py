97774735a2627a80fb6f550ce17416a45f7fca26
nyezza <yezza.nadhir@gmail.com>
2023-02-22 17:21:13 +0100

    v3

--- python/flask/fundamentals/checkerboard/server.py
+++ python/flask/fundamentals/checkerboard/server.py
@@ -1,3 +1,15 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route ('/<int:row>')
+def row_simple(row):
+    return render_template('index.html',y=row//2)
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
