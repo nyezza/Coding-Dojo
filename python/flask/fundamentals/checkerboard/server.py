315501fcd2bface0db6f0bd2bd1e33166f32770d
nyezza <yezza.nadhir@gmail.com>
2023-02-23 16:05:40 +0100

    VF

--- python/flask/fundamentals/checkerboard/server.py
+++ python/flask/fundamentals/checkerboard/server.py
@@ -3,11 +3,16 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    
+    return render_template('home.html')
 
 @app.route ('/<int:row>')
-def row_simple(row):
+def simple_dim(row):
     return render_template('index.html',y=row//2)
+
+@app.route('/<int:row>/<int:col>')
+def double_dim(row,col):
+    return render_template('index.html',y=row,x=col)
 
 
 
