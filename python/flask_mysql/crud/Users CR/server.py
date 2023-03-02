b05f581a64fdf4ba6abc9982fa5f256c97fafe49
nyezza <yezza.nadhir@gmail.com>
2023-03-02 17:24:54 +0100

    VF

--- python/flask_mysql/crud/Users CR/server.py
+++ python/flask_mysql/crud/Users CR/server.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, redirect, request
+from user_model import Users
 
 app=Flask(__name__)
 
-
 @app.route('/')
 def main():
-    return render_template("read.html")
+    return redirect('/user')
+    
+@app.route('/user')
+def all():
+    all_users= Users.read_all()
+    return render_template("read.html",users_list=all_users)
+@app.route('/user/new')
+def new():
+    return render_template("create.html")
+
+@app.route('/user/create', methods=['post'] )
+def create_user():
+    print(request.form)
+    Users.create(request.form)
+    return redirect('/')
 
 
 
--- python/flask_mysql/crud/Users CR/user_model.py
+++ python/flask_mysql/crud/Users CR/user_model.py
@@ -3,18 +3,26 @@ from mysqlconnection import connectToMySQL
 class Users:
     def __init__(self,data):
         self.id= data['id']
-        self.First_Name= data['First_Name']
-        self.Last_Name= data['Last_Name']
-        self.Email= data['Email']
+        self.first_name= data['first_name']
+        self.last_name= data['last_name']
+        self.email= data['email']
         
     @classmethod
     def read_all(cls):
         query="""
-            SELECT * FROM users
+            SELECT * FROM users;
         """
-        result=connectToMySQL("user_db").query_db(query)
+        result=connectToMySQL("users_shema").query_db(query)
         users_list=[]
         for row in result:
             users_list.append(cls(row))
         return users_list
+    
+    @classmethod
+    def create(cls,data):
+        query= """
+            insert into users(first_name,last_name,email) values (%(first_name)s,%(last_name)s,%(email)s);
+        """
+        results = connectToMySQL("users_shema").query_db(query,data)
+        return results
         
