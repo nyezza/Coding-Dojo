25067d8a97d9492059f9627cb8fa2bbe6bc3091d
nyezza <yezza.nadhir@gmail.com>
2023-03-07 06:55:47 +0100

    v5

--- python/flask_mysql/crud/ninjas_and_dojos/flask_app/controllers/dojos.py
+++ python/flask_mysql/crud/ninjas_and_dojos/flask_app/controllers/dojos.py
@@ -1,4 +1,6 @@
 from flask_app import app
 from flask import render_tempalte,redirect,request,session,flash
 from flask_app.models.ninja import Dojo
+from flask_app import DATABASE
+
 
--- python/flask_mysql/crud/ninjas_and_dojos/flask_app/controllers/ninjas.py
+++ python/flask_mysql/crud/ninjas_and_dojos/flask_app/controllers/ninjas.py
@@ -1,3 +1,4 @@
 from flask_app import app
 from flask import render_tempalte,redirect,request,session,flash
 from flask_app.models.ninja import Ninja
+from flask_app import DATABASE
