6766a670d896dd8e9b5ce614ecfdcd6418d4e1e7
nadhir <yezza.nadhir@gmail.com>
2023-04-25 15:29:59 +0100

    daikichi

---
+++ Java/Spring/DaikichiRoutes/src/main/java/com/nadhir/yezza/DaikichiRoutesApplication.java
@@ -0,0 +1,15 @@
+package com.nadhir.yezza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class DaikichiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiRoutesApplication.class, args);
+	}
+	
+}
