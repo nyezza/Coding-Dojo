a8cf86bde3b9045bd459603d71ad56288b9b3f13
nyezza <yezza.nadhir@gmail.com>
2023-05-02 11:54:14 +0100

    test

---
+++ Java/Spring/testJSTL/src/main/java/com/nadhir/yezza/TestJstlApplication.java
@@ -0,0 +1,13 @@
+package com.nadhir.yezza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJstlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJstlApplication.class, args);
+	}
+
+}
