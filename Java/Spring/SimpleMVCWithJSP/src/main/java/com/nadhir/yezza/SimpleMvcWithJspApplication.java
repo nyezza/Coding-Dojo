3e382816d71634fc568c6f65f4a11f3798834975
nyezza <yezza.nadhir@gmail.com>
2023-05-06 11:03:31 +0100

    mvcWithJsp

---
+++ Java/Spring/SimpleMVCWithJSP/src/main/java/com/nadhir/yezza/SimpleMvcWithJspApplication.java
@@ -0,0 +1,13 @@
+package com.nadhir.yezza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMvcWithJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMvcWithJspApplication.class, args);
+	}
+
+}
