58be687a364c7910ab186f05422eb9e8714632db
Nadhir Yezza <N.Yezza@Stunas.local>
2023-05-23 16:31:39 +0100

    preditctDes

--- MERN/Javascript/Fundamentals/hello.js
+++ MERN/Javascript/Fundamentals/hello.js
@@ -1,1 +1,9 @@
 console.log("hello");
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
